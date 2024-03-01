220b75d39da85b44d750eef653dc0b26f5829ac9
Vladyslav <nozhik09@gmail.com>
2024-03-01 18:48:19 +0100

    Version 1.02

--- src/MenuApp.java
+++ src/MenuApp.java
@@ -1,10 +1,16 @@
 import RepositoryLayer.BookRepository;
+import service.BookService;
 import view.Menu;
 
 public class MenuApp {
     public static void main(String[] args) {
-        Menu menu= new Menu();
-        menu.displayMenu();
+        BookRepository bookRepository = new BookRepository();
+        BookService bookService =new BookService(bookRepository);
+
+
+
+
+        Menu menu= new Menu(bookService);
         menu.run();
 
 
