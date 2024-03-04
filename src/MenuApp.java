c20d3bbed8deae7bf71b50d8216cda7081ae2df1
Daniil Zaltur <zaltur_daniil@gmx.de>
2024-03-04 19:22:36 +0100

    MenuGotovo

--- src/MenuApp.java
+++ src/MenuApp.java
@@ -1,31 +1,8 @@
-import RepositoryLayer.BookRepository;
-import RepositoryLayer.UserRepository;
-import service.BookService;
-import service.UserService;
-//import view.Menu;
+import view.Menu;
 
 public class MenuApp {
     public static void main(String[] args) {
-
-
-        BookRepository bookRepository = new BookRepository();
-        BookService bookService =new BookService(bookRepository);
-
-
-
-
-//        Menu menu= new Menu(bookService);
-//
-//        menu.run();
-
-        UserRepository userRepository = new UserRepository();
-        UserService userService = new UserService(userRepository);
-
-        userService.registerUser("test@test.te", "Passw@rd1");
-        System.out.println(userService.getAllUsers());
-
-        System.out.println(userService.authorize("test@test.te", "Passw@rd1"));
-
-
+        Menu menu = new Menu();
+        menu.run();
     }
 }
